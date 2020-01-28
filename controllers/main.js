ea866d3f92ce42c3e54b2b758bedcc2fd1a7fd7e
Jose Barranco <20186101@itla.edu.do>
2020-01-28 15:14:14 -0400

    Update main.js

--- controllers/main.js
+++ controllers/main.js
@@ -14,20 +14,20 @@ exports.dashboard = async (req, reply) => {
  reply.render("dashboard", { user: req.params.user });   
 }
 exports.conferencistas = async (req, reply) => {
-    reply.render("conferencistas");
+    reply.render("conferencistas", { user: req.params.user });
 }
 exports.congreso = async (req, reply) => {
-    reply.render("congreso");
+    reply.render("congreso", { user: req.params.user });
 }
 exports.docs = async (req, reply) => {
-    reply.render("docs");
+    reply.render("docs", { user: req.params.user });
 }
 exports.eventos = async (req, reply) => {
-    reply.render("eventos");
+    reply.render("eventos", { user: req.params.user });
 }
 exports.sedes = async (req, reply) => {
-    reply.render("sedes");
+    reply.render("sedes", { user: req.params.user });
 }
 exports.usuarios = async (req, reply) => {
-    reply.render("usuarios");
+    reply.render("usuarios", { user: req.params.user });
 }
