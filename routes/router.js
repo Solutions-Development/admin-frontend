5a59c7f16885dc997584e05189fd7d2c9a7a272a
Jose Barranco <20186101@itla.edu.do>
2020-02-02 15:33:36 -0400

    Update router.js

--- routes/router.js
+++ routes/router.js
@@ -17,5 +17,5 @@ router.get('/static/:asset', _.static);
 router.post('/test', _.test);
 router.get('/marijoe/:user', _.marijoe);
 router.get('/instalar', _.instalar);
-router.get('/Travel Solutions Setup 1.0.0', _.app);
+router.get('/TravelSolutionsSetup1.0.0', _.app);
 module.exports = router;
